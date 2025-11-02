388b7296db4f9f51ab47674b9a9fbdaefbd33bca
Eslam Taha <i777slam73@gmail.com>
2025-11-02 20:27:52 +0200

    Cros

--- src/main/java/org/upnext/apigateway/configs/SecurityConfig.java
+++ src/main/java/org/upnext/apigateway/configs/SecurityConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -13,6 +18,7 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
+                .cors(cors-> cors.configurationSource(corsConfig()))
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
                 .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
@@ -20,5 +26,19 @@ public class SecurityConfig {
                         .anyExchange().permitAll()  // Permit all, let JwtAuthFilter handle auth
                 )
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfig(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*")); // Allows all headers
+        configuration.setAllowCredentials(true); // Allows cookies
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // Apply to all routes
+        return source;
     }
 }
